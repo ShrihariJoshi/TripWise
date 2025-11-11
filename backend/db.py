77fc551b8cd4848604d11ffac2b9df8f25c7ad5b
swanand11 <swanandgadwe@gmail.com>
2025-11-11 23:05:05 +0530

    Initial commit: clean branch Sg with .gitignore fixed

---
+++ backend/db.py
@@ -0,0 +1,77 @@
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_db_connection():
+    conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+    return conn
+def create_tables():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(150) NOT NULL,
+        email VARCHAR(150) UNIQUE NOT NULL,
+        phone VARCHAR(10),
+        password_hash VARCHAR(255) NOT NULL
+    );
+    CREATE TABLE IF NOT EXISTS trips (
+        trip_id SERIAL PRIMARY KEY,
+        trip_name VARCHAR(255) NOT NULL,
+        start_date DATE NOT NULL,
+        end_date DATE NOT NULL,
+        destination VARCHAR(255) NOT NULL,
+        created_by INTEGER REFERENCES users(id),
+        trip_budget FLOAT
+    );
+    CREATE TABLE IF NOT EXISTS TripMember (
+        member_id SERIAL PRIMARY KEY,
+        user_id INTEGER REFERENCES users(id),
+        trip_id INTEGER REFERENCES trips(trip_id),
+        role VARCHAR(50) NOT NULL
+    );
+    CREATE TABLE IF NOT EXISTS Expenses (
+        expense_id SERIAL PRIMARY KEY,
+        trip_id INTEGER REFERENCES trips(trip_id),
+        Paid_by INTEGER REFERENCES users(id),
+        description TEXT,      
+        amount FLOAT NOT NULL,
+        date DATE NOT NULL
+    );
+    CREATE TABLE IF NOT EXISTS ExpenseShare (
+        share_id SERIAL PRIMARY KEY,
+        expense_id INTEGER REFERENCES Expenses(expense_id),
+        user_id INTEGER REFERENCES users(id),
+        share_amount FLOAT NOT NULL,
+        status VARCHAR(50) NOT NULL
+    );
+    CREATE TABLE IF NOT EXISTS Itineary (
+        itineary_id SERIAL PRIMARY KEY,
+        trip_id INTEGER REFERENCES trips(trip_id),
+        day_number INTEGER NOT NULL,
+        title VARCHAR(255) NOT NULL,
+        description TEXT
+    );
+    CREATE TABLE IF NOT EXISTS Activity (
+        activity_id SERIAL PRIMARY KEY,
+        itineary_id INTEGER REFERENCES Itineary(itineary_id),
+        start_Time TIME NOT NULL,
+        end_time TIME NOT NULL,
+        location VARCHAR(255) NOT NULL,
+        notes TEXT NOT NULL
+    );
+    CREATE TABLE IF NOT EXISTS Settlement (
+                settlement_id SERIAL PRIMARY KEY,
+                trip_id INTEGER REFERENCES trips(trip_id),
+                from_user INTEGER REFERENCES users(id),
+                to_user INTEGER REFERENCES users(id),
+                amount FLOAT NOT NULL,
+                date DATE NOT NULL
+    );
+    """)
+    conn.commit()
+    cur.close()
+    conn.close()
---
+++ backend/endpoints/auth.py
@@ -0,0 +1,41 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask import request, jsonify
+from flask_jwt_extended import create_access_token
+from db import get_db_connection
+
+def register_handler():
+    data = request.get_json() or {}
+    username = data.get("username")
+    email = data.get("email")
+    phone = data.get("phone")
+    password = data.get("password")
+    if not (username and email and phone and password):
+        return jsonify(message="missing fields"), 400
+    hashed = generate_password_hash(password)
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute(
+        "INSERT INTO users (username, email, phone, password_hash) VALUES (%s, %s, %s, %s)",
+        (username, email, phone, hashed)
+    )
+    conn.commit()
+    cur.close()
+    conn.close()
+    return jsonify(message="User registered successfully"), 201
+
+def login_handler():
+    data = request.get_json() or {}
+    email = data.get("email")
+    password = data.get("password")
+    if not (email and password):
+        return jsonify(message="missing fields"), 400
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT id, password_hash FROM users WHERE email = %s", (email,))
+    user = cur.fetchone()
+    cur.close()
+    conn.close()
+    if not user or not check_password_hash(user[1], password):
+        return jsonify(message="Invalid credentials"), 401
+    token = create_access_token(identity=user[0])
+    return jsonify(access_token=token)
