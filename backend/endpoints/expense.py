e90d7c3727c63eae713e5e5f627320c498019303
swanand11 <swanandgadwe@gmail.com>
2025-11-28 11:25:26 +0530

    expenses.v.0

--- backend/endpoints/expense.py
+++ backend/endpoints/expense.py
@@ -5,18 +5,21 @@ from db import get_db_connection
 
 def expense_handler():
     data = request.get_json() or {}
-    trip_id = data.get("trip_id")
+    trip_name = data.get("trip_name")
     amount = data.get("amount")
     description = data.get("description")
     paid_by_username = data.get("paid_by")
     date = data.get("date")
 
-    if not (trip_id and amount and description and paid_by_username):
+    if not (trip_name and amount and description and paid_by_username):
         return jsonify(message="missing fields"), 400
 
     conn = get_db_connection()
     cur = conn.cursor()
-    paid_by = cur.execute("SELECT id FROM users WHERE username = %s", (paid_by_username,))
+    cur.execute("SELECT trip_id FROM trips WHERE trip_name = %s", (trip_name,))
+    trip_id = cur.fetchone()
+    cur.execute("SELECT id FROM users WHERE username = %s", (paid_by_username,))
+    paid_by = cur.fetchone()
     cur.execute(
         "INSERT INTO expenses (trip_id, amount, description, paid_by,date) VALUES (%s, %s, %s, %s,%s)",
         (trip_id, amount, description, paid_by, date)
@@ -34,9 +37,11 @@ def expense_share_handler():
     conn = get_db_connection()
     cur = conn.cursor()
     id_list=[]
-    amount=cur.execute("SELECT amount FROM Expenses WHERE expense_id = %s", (expense_id,))
+    cur.execute("SELECT amount FROM Expenses WHERE expense_id = %s", (expense_id,))
+    amount=cur.fetchone()[0]
     for user in users_list:
-        user_id = cur.execute("SELECT id FROM users WHERE username = %s", (user,))
+        cur.execute("SELECT id FROM users WHERE username = %s", (user,))
+        user_id = cur.fetchone()
         id_list.append(user_id)
     for uid in id_list:
         share_amount=amount/len(id_list)
